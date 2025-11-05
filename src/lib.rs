a669c19e51d10d6257f6c9a84ba30437b4865267
Padraigh Looney <padraighl@Padraigh-Looneys-MacBook-Pro-M4-Max-silicon-2025-model.local>
2025-11-05 10:06:43 +0000

    Adding conversion functions between np arrays and geo types

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,26 @@
 use ndarray::parallel::prelude::ParallelIterator;
-use numpy::ndarray::{Axis, Array1};
-use numpy::{PyArray1, PyReadonlyArray1, PyReadonlyArray2, IntoPyArray};
+use numpy::ndarray::{Array1, Axis};
+use numpy::{IntoPyArray, PyArray1, PyArray2, PyReadonlyArray1, PyReadonlyArray2};
 
-use geo::{Point, LineString, Distance, Euclidean};
-use ndarray::{ArrayView1, ArrayView2};
+use geo::{Distance, Euclidean, LineString, Point, Polygon, unary_union};
 use ndarray::parallel::prelude::IntoParallelIterator;
-use pyo3::{pymodule, types::{PyModule}, Bound, PyResult, Python};
+use ndarray::{ArrayView1, ArrayView2};
+use pyo3::{
+    Bound, PyResult, Python, pymodule,
+    types::{PyDict, PyList, PyModule},
+};
 
 #[pymodule]
 #[pyo3(name = "rust_geo_python")]
 fn polygon<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
-
     fn point_poly_distance(x: ArrayView1<f64>, y: ArrayView2<f64>) -> f64 {
-        let path = y.axis_iter(Axis(0))
+        let path = y
+            .axis_iter(Axis(0))
             .map(|x| Point::new(x[0], x[1]))
             .collect::<LineString>();
         let point = Point::new(x[0], x[1]);
-        let distance = Euclidean.distance(&point,&path);
+        let distance = Euclidean.distance(&point, &path);
         distance
-
     }
 
     #[pyfn(m)]
@@ -42,8 +44,9 @@ fn polygon<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
     ) -> Bound<'py, PyArray1<f64>> {
         let x = x.as_array();
         let y = y.as_array();
-        let distances = x.axis_iter(Axis(0))
-            .map(|p| point_poly_distance(p,y))
+        let distances = x
+            .axis_iter(Axis(0))
+            .map(|p| point_poly_distance(p, y))
             .collect::<Array1<f64>>();
         distances.into_pyarray(py)
     }
@@ -63,7 +66,7 @@ fn polygon<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
             .axis_iter(Axis(0))
             .map(|x| Point::new(x[0], x[1]))
             .collect::<LineString>();
-        let distance = Euclidean.distance(&path_x,&path_y);
+        let distance = Euclidean.distance(&path_x, &path_y);
         distance
     }
 
@@ -76,10 +79,49 @@ fn polygon<'py>(m: &Bound<'py, PyModule>) -> PyResult<()> {
     ) -> Bound<'py, PyArray1<f64>> {
         let x = x.as_array();
         let y = y.as_array();
-        let distances_vec = x.axis_iter(Axis(0)).into_par_iter()
-            .map(|p| point_poly_distance(p,y))
+        let distances_vec = x
+            .axis_iter(Axis(0))
+            .into_par_iter()
+            .map(|p| point_poly_distance(p, y))
             .collect::<Vec<f64>>();
         distances_vec.into_pyarray(py)
+    }
+
+    fn array2_to_linestring<'py>(x: &PyReadonlyArray2<'py, f64>) -> LineString {
+        let path = x
+            .as_array()
+            .axis_iter(Axis(0))
+            .map(|y| Point::new(y[0], y[1]))
+            .collect::<LineString>();
+        path
+    }
+
+    fn array2_to_polygon<'py>(
+        x: &PyReadonlyArray2<'py, f64>,
+        ys: &Vec<PyReadonlyArray2<'py, f64>>,
+    ) -> Polygon {
+        let exterior = array2_to_linestring(&x);
+        let interiors = ys
+            .iter()
+            .map(|y| array2_to_linestring(y))
+            .collect::<Vec<LineString>>();
+        Polygon::new(exterior, interiors)
+    }
+
+    fn polygon_to_array2<'py>(polygons: &Vec<Polygon>) -> () {}
+
+    #[pyfn(m)]
+    #[pyo3(name = "union_set_shapes")]
+    fn union_set_shapes<'py>(
+        py: Python<'py>,
+        pyarrays: Vec<(PyReadonlyArray2<'py, f64>, Vec<PyReadonlyArray2<'py, f64>>)>,
+        //) -> PyList<(PyArray2, PyList<PyArray2>)> {
+    ) -> () {
+        let polygons = pyarrays
+            .iter()
+            .map(|(x, ys)| array2_to_polygon(x, ys))
+            .collect::<Vec<Polygon>>();
+        let union = unary_union(&polygons);
     }
 
     Ok(())
