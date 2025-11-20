dbd852c04fad9bc1d1067d19a38e7622615a3be6
Padraigh Looney <padraig.looney@well.ox.ac.uk>
2025-11-20 14:28:15 +0000

    Adding constructors and functions for distance, intersection, union

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,15 @@ mod rust_geo_python {
     use numpy::ndarray::{Array1, Array2, Axis};
     use numpy::{IntoPyArray, PyArray1, PyArray2, PyReadonlyArray1, PyReadonlyArray2};
 
-    use geo::{BooleanOps, Distance, Euclidean, LineString, Point, Polygon, unary_union};
+    use geo::{
+        BooleanOps, Distance, Euclidean, LineString, MultiLineString, MultiPolygon, Point, Polygon,
+        unary_union,
+    };
     use ndarray::parallel::prelude::IntoParallelIterator;
     use ndarray::{ArrayView1, ArrayView2};
     use pyo3::prelude::*;
     use pyo3::{Bound, PyResult, Python};
+    use std::sync::Arc;
 
     fn point_poly_distance(x: ArrayView1<f64>, y: ArrayView2<f64>) -> f64 {
         let path = y
@@ -170,5 +174,197 @@ mod rust_geo_python {
         let polygon_y = array2_to_polygon(&pyarray_y.0, &pyarray_y.1);
         let intersection = polygon_x.difference(&polygon_y);
         polygons_to_array2(py, intersection.iter().collect::<Vec<&Polygon>>())
+    }
+
+    #[derive(Clone)]
+    enum Shapes {
+        Point(Point),
+        LineString(LineString),
+        MultiLineString(MultiLineString),
+        Polygon(Arc<Polygon>),
+        MultiPolygon(MultiPolygon),
+    }
+
+    #[pyclass(subclass)]
+    #[derive(Clone)]
+    struct Shape {
+        inner: Shapes,
+    }
+
+    #[pyclass(extends=Shape)]
+    #[derive(Clone)]
+    struct RustPoint {}
+    #[pyclass(extends=Shape)]
+    struct RustLineString {}
+    #[pyclass(extends=Shape)]
+    #[derive(Clone)]
+    struct RustPolygon {
+        polygon: Arc<Polygon>,
+    }
+    #[pyclass(extends=Shape)]
+    struct RustMultiLineString {}
+
+    #[pyclass(extends=Shape)]
+    struct RustMultiPolygon {}
+
+    #[pymethods]
+    impl RustLineString {
+        #[new]
+        fn new(x: PyReadonlyArray2<f64>) -> (Self, Shape) {
+            let ls = array2_to_linestring(&x);
+            (
+                RustLineString {},
+                Shape {
+                    inner: Shapes::LineString(ls),
+                },
+            )
+        }
+    }
+
+    #[pymethods]
+    impl RustPoint {
+        #[new]
+        fn new(x: f64, y: f64) -> (Self, Shape) {
+            (
+                RustPoint {},
+                Shape {
+                    inner: Shapes::Point(Point::new(x, y)),
+                },
+            )
+        }
+    }
+
+    #[pymethods]
+    impl RustPolygon {
+        #[new]
+        fn new(x: PyReadonlyArray2<f64>, ys: Vec<PyReadonlyArray2<f64>>) -> (Self, Shape) {
+            let polygon = array2_to_polygon(&x, &ys);
+            let polygon_arc = Arc::new(polygon);
+            (
+                RustPolygon {
+                    polygon: polygon_arc.clone(),
+                },
+                Shape {
+                    inner: Shapes::Polygon(polygon_arc.clone()),
+                },
+            )
+        }
+    }
+
+    #[pymethods]
+    impl RustMultiLineString {
+        #[new]
+        fn new(ys: Vec<PyReadonlyArray2<f64>>) -> (Self, Shape) {
+            let lss = ys
+                .iter()
+                .map(|x| array2_to_linestring(x))
+                .collect::<MultiLineString>();
+            (
+                RustMultiLineString {},
+                Shape {
+                    inner: Shapes::MultiLineString(lss),
+                },
+            )
+        }
+    }
+
+    #[pymethods]
+    impl RustMultiPolygon {
+        #[new]
+        fn new(
+            pyarrays: Vec<(PyReadonlyArray2<f64>, Vec<PyReadonlyArray2<f64>>)>,
+        ) -> (Self, Shape) {
+            let polygons = pyarrays
+                .iter()
+                .map(|(x, ys)| array2_to_polygon(x, ys))
+                .collect::<Vec<Polygon>>();
+            (
+                RustMultiPolygon {},
+                Shape {
+                    inner: Shapes::MultiPolygon(MultiPolygon(polygons)),
+                },
+            )
+        }
+    }
+
+    #[pymethods]
+    impl Shape {
+        fn distance(&self, rhs: &Shape) -> f64 {
+            match (&self.inner, &rhs.inner) {
+                (Shapes::Point(p), Shapes::Point(q)) => Euclidean.distance(p, q),
+                (Shapes::LineString(p), Shapes::Point(q)) => Euclidean.distance(p, q),
+                (Shapes::Point(p), Shapes::LineString(q)) => Euclidean.distance(p, q),
+                (Shapes::LineString(p), Shapes::LineString(q)) => Euclidean.distance(p, q),
+                (Shapes::MultiLineString(p), Shapes::Point(q)) => Euclidean.distance(p, q),
+                (Shapes::MultiLineString(p), Shapes::LineString(q)) => Euclidean.distance(p, q),
+                (Shapes::MultiLineString(p), Shapes::MultiLineString(q)) => {
+                    Euclidean.distance(p, q)
+                }
+                (Shapes::Point(p), Shapes::MultiLineString(q)) => Euclidean.distance(p, q),
+                (Shapes::LineString(p), Shapes::MultiLineString(q)) => Euclidean.distance(p, q),
+                (Shapes::Polygon(p), Shapes::Point(q)) => Euclidean.distance(p.as_ref(), q),
+                (Shapes::Polygon(p), Shapes::LineString(q)) => Euclidean.distance(p.as_ref(), q),
+                (Shapes::Polygon(p), Shapes::MultiLineString(q)) => {
+                    Euclidean.distance(p.as_ref(), q)
+                }
+                (Shapes::Polygon(p), Shapes::Polygon(q)) => {
+                    Euclidean.distance(p.as_ref(), q.as_ref())
+                }
+                (Shapes::Point(p), Shapes::Polygon(q)) => Euclidean.distance(p, q.as_ref()),
+                (Shapes::LineString(p), Shapes::Polygon(q)) => Euclidean.distance(p, q.as_ref()),
+                (Shapes::MultiLineString(p), Shapes::Polygon(q)) => {
+                    Euclidean.distance(p, q.as_ref())
+                }
+                (Shapes::MultiPolygon(p), Shapes::Point(q)) => Euclidean.distance(p, q),
+                (Shapes::MultiPolygon(p), Shapes::LineString(q)) => Euclidean.distance(p, q),
+                (Shapes::MultiPolygon(p), Shapes::MultiLineString(q)) => Euclidean.distance(p, q),
+                (Shapes::MultiPolygon(p), Shapes::Polygon(q)) => Euclidean.distance(p, q.as_ref()),
+                (Shapes::MultiPolygon(p), Shapes::MultiPolygon(q)) => Euclidean.distance(p, q),
+                (Shapes::Point(p), Shapes::MultiPolygon(q)) => Euclidean.distance(p, q),
+                (Shapes::LineString(p), Shapes::MultiPolygon(q)) => Euclidean.distance(p, q),
+                (Shapes::MultiLineString(p), Shapes::MultiPolygon(q)) => Euclidean.distance(p, q),
+                (Shapes::Polygon(p), Shapes::MultiPolygon(q)) => Euclidean.distance(p.as_ref(), q),
+            }
+        }
+    }
+
+    // ys: &Vec<PyReadonlyArray2<'py, f64>>,
+    #[pyfunction]
+    fn union<'py>(py: Python<'py>, rust_polygons: Vec<RustPolygon>) -> PyResult<Py<PyAny>> {
+        let polygons = rust_polygons
+            .iter()
+            .map(|x| x.polygon.as_ref())
+            .collect::<Vec<&Polygon>>();
+        let union = unary_union(polygons);
+        let initializer: PyClassInitializer<RustMultiPolygon> = PyClassInitializer::from((
+            RustMultiPolygon {},
+            Shape {
+                inner: Shapes::MultiPolygon(union),
+            },
+        ));
+        Ok(Py::new(py, initializer)?.into_any())
+    }
+    #[pyfunction]
+    fn count<'py>(py: Python<'py>, rust_points: Vec<RustPoint>) -> PyResult<()> {
+        println!("Some text {}", rust_points.len());
+        Ok(())
+    }
+
+    #[pyfunction(name = "intersection")]
+    fn intersection<'py>(
+        py: Python<'py>,
+        polygon_lhs: &RustPolygon,
+        polygon_rhs: &RustPolygon,
+    ) -> PyResult<Py<PyAny>> {
+        let intersection = polygon_lhs
+            .polygon
+            .intersection(polygon_rhs.polygon.as_ref());
+        let initializer: PyClassInitializer<RustMultiPolygon> = PyClassInitializer::from((
+            RustMultiPolygon {},
+            Shape {
+                inner: Shapes::MultiPolygon(intersection),
+            },
+        ));
+        Ok(Py::new(py, initializer)?.into_any())
     }
 }
