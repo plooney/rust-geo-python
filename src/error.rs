a3fa0f090877a3f87be0fe6a13bad575684f3924
Padraigh Looney <padraig.looney@well.ox.ac.uk>
2026-02-06 15:02:03 +0000

    Refactoring errors

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,13 @@ pub fn validate_xy_dimensions(shape: &[usize]) -> PyResult<()> {
     Ok(())
 }
 
+pub fn validate_point_dimensions(shape: &[usize]) -> PyResult<()> {
+    if shape.len() != 1 || shape[0] != 2 {
+        return Err(PyValueError::new_err("point array must have shape (2,)"));
+    }
+    Ok(())
+}
+
 pub fn validate_non_empty(shape: &[usize], name: &str) -> PyResult<()> {
     if shape.is_empty() || shape[0] == 0 {
         return Err(PyValueError::new_err(format!("{name} must be non-empty")));
